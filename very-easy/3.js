76640b7911f0b6db87cf2f13ad548bfc4fd0c081
Gabriel Macena <gabriel.macena10@hotmail.com>
2024-09-01 17:13:23 -0300

    Desafio 03

---
+++ very-easy/3.js
@@ -0,0 +1,10 @@
+function inverter(arr) {
+  const arrayInvertido = [];
+
+  for (let i = arr.length - 1; i >= 0; i--) {
+    arrayInvertido.push(arr[i]);
+  }
+  return arrayInvertido;
+}
+
+console.log(inverter([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
